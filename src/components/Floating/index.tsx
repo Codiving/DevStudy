abe9722dee0369254d3ec5e0bebee00d8b45246f
Codiving <codivingmoon@gmail.com>
2024-02-11 16:03:02 +0900

    feat: ArrowUp 버튼 클릭 시 상단으로 이동 기능 추가

--- src/components/Floating/index.tsx
+++ src/components/Floating/index.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import { useCallback } from "react";
 import { IoArrowUp } from "react-icons/io5";
 import styles from "./Floating.module.css";
 
 const Floating = () => {
+  const onMoveTop = useCallback(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }, []);
+
   return (
-    <aside className={styles.container}>
+    <aside className={styles.container} onClick={onMoveTop}>
       <IoArrowUp className={styles.button} size={24} />
     </aside>
   );
